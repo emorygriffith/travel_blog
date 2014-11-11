e5af19ca4b63bd88ec37e71da66f7261f87bc58e
Emory Griffith <emorygriffith91@gmail.com>
2014-11-11 12:35:50 -0500

    setup model

---
+++ app/scripts/models/post_model.js
@@ -0,0 +1,23 @@
+(function () {
+
+  App.Model.extend = Parse.Object.extend({
+
+    // className: 'Post',
+
+    idAttribute: 'objectId',
+
+    defaults: {
+      title: '',
+      content: '',
+      category: '',
+      status: ''
+    },
+
+    initialize: function(){
+      var t = this.get('title');
+      console.log(t + 'has been added');
+    }
+
+  });
+
+}());
